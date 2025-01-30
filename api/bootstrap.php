504866a51d82a9552b0b449a0c4b87c9eefd2ce9
erickdek <gerhoye0@gmail.com>
2025-01-29 23:37:36 -0500

    update the name files php

--- api/bootstrap.php
+++ api/bootstrap.php
@@ -1,29 +1,29 @@
 <?php
 
 // Utils
-require_once __DIR__ . '/Utils/Validator.php';
+require_once __DIR__ . '/utils/Validator.php';
 
 // Models
-require_once __DIR__ . '/Models/User.php';
+require_once __DIR__ . '/models/User.php';
 
 // Config
-require_once __DIR__ . '/Config/JWTConfig.php';
-require_once __DIR__ . '/Config/Database.php';
+require_once __DIR__ . '/config/JWTConfig.php';
+require_once __DIR__ . '/config/Database.php';
 
 // Controllers
-require_once __DIR__ . '/Controllers/HomeController.php';
-require_once __DIR__ . '/Controllers/StatusController.php';
-require_once __DIR__ . '/Controllers/AuthController.php';
+require_once __DIR__ . '/controllers/HomeController.php';
+require_once __DIR__ . '/controllers/StatusController.php';
+require_once __DIR__ . '/controllers/AuthController.php';
 
 // Middleware
-require_once __DIR__ . '/Middleware/RateLimitMiddleware.php';
+require_once __DIR__ . '/middleware/RateLimitMiddleware.php';
 
 // Services
-require_once __DIR__ . '/Services/LoggerService.php';
-require_once __DIR__ . '/Services/AuthService.php';
+require_once __DIR__ . '/services/LoggerService.php';
+require_once __DIR__ . '/services/AuthService.php';
 
 // Routes
-require_once __DIR__ . '/Routes/Router.php';
+require_once __DIR__ . '/routes/Router.php';
 
 // Dotenv
 require_once __DIR__ . '/vendor/autoload.php';
